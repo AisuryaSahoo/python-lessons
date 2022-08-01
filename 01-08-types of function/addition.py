fd5a312df3287be3d8ed5c219f1bc16de1d7f45a
Aisurya <aisurya.sahoo@gmail.com>
2022-08-01 20:35:45 +0530

    01-08/types of function

---
+++ 01-08-types of function/addition.py
@@ -0,0 +1,12 @@
+def add(p,q):#1
+    r=p+q#2
+    return r#3
+x=10#4
+y=20#5
+z=add(x,y)#6  call-1
+print("result=",z)#7
+print("3+4=",add(3,4)) #call-2
+"""
+memory occupied by function definition will
+be cleared after its(function) execution ends
+"""
