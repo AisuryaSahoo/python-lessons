c368bb0468bdc76d88211ee5c43ff735c763d1f4
Aisurya <aisurya.sahoo@gmail.com>
2022-08-22 20:07:00 +0530

    if and for

---
+++ python if & for.py
@@ -0,0 +1,73 @@
+#even-odd
+"""
+a=11
+if(a%2==0):
+    print(a,"is an even number")
+else:
+    print(a,"is an odd number")
+"""
+
+#compare 2 nos
+"""
+a=int(input("Enter 1st number"))
+
+b=int(input("Enter 2nd number"))
+
+if(a>b):
+    print(a,"is greater")
+elif(b>a):
+    print(b,"is greater")
+else:
+    print("both are equal")
+"""
+
+#compare 3 nos
+
+
+"""
+a=int(input("Enter 1st number"))
+
+b=int(input("Enter 2nd number"))
+c=int(input("Enter 3rd number"))
+
+if(a>b):
+    if(a>c):
+        print(a,"is greatest")
+    else:
+        print(c,"is greatest")
+else:
+    if(b>c):
+        print(b,"is greatest")
+    else:
+        print(c,"is greatest")
+
+"""
+
+#for loop
+"""
+for i in [1,3,5,6,8]:
+    print("hello world")
+
+"""
+#print 1 to 10
+"""
+for i in (1,2,3,4,5,6,7,8,9,10):
+    print(i)
+"""
+#for with string
+"""
+for i in "hello":
+    print(i)
+"""
+#for with range()
+"""
+for i in range(5):
+    print(i)
+"""
+"""
+for i in range(5,10):
+    print(i)
+"""
+for i in range(5,50,5):
+    print(i)
+
